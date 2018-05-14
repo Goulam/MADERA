4a222cf742f78cd142d810209efee4c058266cf5
Unknown <benoit.bourcier@viacesi.fr>
2018-05-14 16:08:27 +0200

    /Devis: Auto stash before revert of "Merge pull request #2 from Goulam/Feature/BDDAZURE"

---
+++ Web Service/Web Service/BLL/GammesBLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Madera.ClassLibrary.Model;
+using Madera.ClassLibrary.Entity;
+
+namespace Madera.ClassLibrary.BLL
+{
+    public class GammesBLL : GammesDAO
+    {
+        public List<GammesEntity> selectAllGames()
+        {
+            return selectAllGamme();
+        }
+    }
+}
